d199cbce416bc2694d37ad3d51a290cdc568684e
Marc Lainez <marc.lainez@gmail.com>
2018-03-02 19:13:40 +0100

    Add error handling and parse response as json

---
+++ ibanity/Error.py
@@ -0,0 +1,12 @@
+class Error(Exception):
+    def __init__(self, errors):
+        self.errors = errors
+
+    def str(self):
+        str(map(lambda error: self.__format_error(error), self.errors))
+
+    def __format_error(error):
+        if error["attribute"]:
+            return "* " + error["code"] + ": " + error["attribute"] + " " + error["message"]
+        else:
+            return "* " + error["code"] + error["message"]
